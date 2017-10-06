830e1dc7fa965d5d33a3669d7254fc06981e506d
1412578 - Trần Quang Tri <tranquangtri96v2@gmail.com>
2017-10-06 14:11:56 +0700

    * update albums page

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,4 @@
 class Album < ApplicationRecord
 	has_many :pictures
+	
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@ album = Album.create([
 	{cover_url: "https://i.imgur.com/ij16QvY.jpg", author: "Trần Hông Thuận", view_count: 0},
 	{cover_url: "https://i.imgur.com/hztmlh2.jpg", author: "Mericurus", view_count: 0}])
 Picture.create([
-	{picture_url:"https://i.imgur.com/qYsZJI0m.jpg", uploader: "Trí", view_count: 0, album: album[0]},
-	{picture_url:"https://i.imgur.com/z0IAXyhm.jpg", uploader: "Trí", view_count: 0, album: album[0]},
-	{picture_url:"https://i.imgur.com/W6Z6I2wm.jpg", uploader: "Trí", view_count: 0, album: album[0]},
-	{picture_url:"https://i.imgur.com/foguNdMm.jpg", uploader: "Trí", view_count: 0, album: album[0]},
-	{picture_url:"https://i.imgur.com/WQObSf5m.jpg", uploader: "Vũ", view_count: 0, album: album[1]}])
+	{picture_url:"https://i.imgur.com/qYsZJI0.jpg", uploader: "Trí", view_count: 0, album: album[0]},
+	{picture_url:"https://i.imgur.com/z0IAXyh.jpg", uploader: "Trí", view_count: 0, album: album[0]},
+	{picture_url:"https://i.imgur.com/W6Z6I2w.jpg", uploader: "Trí", view_count: 0, album: album[0]},
+	{picture_url:"https://i.imgur.com/foguNdM.jpg", uploader: "Trí", view_count: 0, album: album[0]},
+	{picture_url:"https://i.imgur.com/WQObSf5.jpg", uploader: "Vũ", view_count: 0, album: album[1]}])
