0a7fe19aa0b7820f4457bf0d8e313defd2b85215
1412578 - Trần Quang Tri <tranquangtri96v2@gmail.com>
2017-10-07 19:34:59 +0700

    + update UI, decorate homepage, decorate leftmenu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def initialize
   	@breadcums = [{title: "Home", url: "/"}]
+  	@view_options = {homepage: false}
   	super()
   end
 end
